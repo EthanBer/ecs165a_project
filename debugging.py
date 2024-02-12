cd9a8b7f5dcb79afd6c816a593723a9c09eb338a
Carlos <credondorubio@ucdavis.edu>
2024-02-12 15:34:11 -0800

    fixed bug in the update

--- debugging.py
+++ debugging.py
@@ -41,60 +41,61 @@ def debugger_2(db: Database) -> None:
 #                 for l in range(len(current_tail_page.physical_pages)):
 #                     print(current_tail_page.physical_pages[l].data)
 
+if __name__ == "__main__":
 
-db = Database()
-# Create a table  with 5 columns
-#   Student Id and 4 grades
-#   The first argument is name of the table
-#   The second argument is the number of columns
-#   The third argument is determining the which columns will be primay key
-#       Here the first column would be student id and primary key
-grades_table = db.create_table('Grades', 5, DataIndex(0))
+    db = Database()
+    # Create a table  with 5 columns
+    #   Student Id and 4 grades
+    #   The first argument is name of the table
+    #   The second argument is the number of columns
+    #   The third argument is determining the which columns will be primay key
+    #       Here the first column would be student id and primary key
+    grades_table = db.create_table('Grades', 5, DataIndex(0))
 
-# create a query class for the grades table
-query = Query(grades_table)
+    # create a query class for the grades table
+    query = Query(grades_table)
 
-# dictionary for records to test the database: test directory
-records = {}
-keys = []
+    # dictionary for records to test the database: test directory
+    records = {}
+    keys = []
 
-number_of_records = 1
-seed(3562901)
+    number_of_records = 1
+    seed(3562901)
 
-for i in range(0, number_of_records):
-    key = 92106429 + randint(0, number_of_records)
-
-    # skip duplicate keys
-    while key in records:
+    for i in range(0, number_of_records):
         key = 92106429 + randint(0, number_of_records)
 
-    records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
-    keys.append(key)
-    query.insert(*records[key])
-    print(key, records)
-print("insert finished")
-print(db.tables[0].get_record_by_rid(1))
-# debugger_2(db)
+        # skip duplicate keys
+        while key in records:
+            key = 92106429 + randint(0, number_of_records)
+
+        records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
+        keys.append(key)
+        query.insert(*records[key])
+        print(key, records)
+    print("insert finished")
+    print(db.tables[0].get_record_by_rid(1))
+    # debugger_2(db)
 
 
-updated_records = {}
-for key in records:
-    updated_columns: list[int | None] = [None, None, None, None, None]
-    updated_records[key] = records[key].copy()
-    for i in range(2, grades_table.num_columns):
-        # updated value
-        value = randint(0, 20)
-        updated_columns[i] = value
-        # update our test directory
-        updated_records[key][i] = value
-    print(f"columns should be updated to {updated_columns}")
-    query.update(key, *updated_columns)
-print("update finished. records:")
-print(updated_records)
-# debugger_2(db)
-print(db.tables[0].get_record_by_rid(1))
-print(db.tables[0].get_record_by_rid(2))
-print(db.tables[0].get_record_by_rid(3))
+    updated_records = {}
+    for key in records:
+        updated_columns: list[int | None] = [None, None, None, None, None]
+        updated_records[key] = records[key].copy()
+        for i in range(2, grades_table.num_columns):
+            # updated value
+            value = randint(0, 20)
+            updated_columns[i] = value
+            # update our test directory
+            updated_records[key][i] = value
+        print(f"columns should be updated to {updated_columns}")
+        query.update(key, *updated_columns)
+    print("update finished. records:")
+    print(updated_records)
+    # debugger_2(db)
+    print(db.tables[0].get_record_by_rid(1))
+    print(db.tables[0].get_record_by_rid(2))
+    print(db.tables[0].get_record_by_rid(3))
 
-print(helper.str_each_el(query.select(keys[0], DataIndex(0), [1] * 5)))
-# print(f"delete successful, key: {keys[0]}" if query.delete(keys[0]) else "delete failed")
+    print(helper.str_each_el(query.select(keys[0], DataIndex(0), [1] * 5)))
+    # print(f"delete successful, key: {keys[0]}" if query.delete(keys[0]) else "delete failed")
--- test.py
+++ test.py
@@ -1,4 +1,53 @@
-def foo(a: int, *arr: tuple[int, ...]) -> int:
-    return 3
+from lstore.db import Database
+from lstore.query import Query
+from random import choice, randint, sample, seed
 
-print(foo(1, 3, 4, 2))
+
+from debugging import debugger_2
+from lstore.ColumnIndex import DataIndex, RawIndex
+
+
+
+
+##################    TEST DELETE   ##########################################
+# make sure that is flagged as deleted in the Base page
+# make sure that is flagged as deleted in  all the Tail Pages
+# make sure is delted from the page_directory of the table ?????????????
+# 
+
+
+
+db = Database()
+grades_table = db.create_table('Grades', 5, DataIndex(0))
+# create a query class for the grades table
+query = Query(grades_table)
+
+# dictionary for records to test the database: test directory
+records = {}
+keys = []
+
+number_of_records = 2
+seed(3562901)
+
+for i in range(0, number_of_records):
+    key = 92106429 + randint(0, number_of_records)
+
+    # skip duplicate keys
+    while key in records:
+        key = 92106429 + randint(0, number_of_records)
+
+    records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
+    keys.append(key)
+    query.insert(*records[key])
+    print(key, records)
+print("insert finished")
+print(db.tables[0].get_record_by_rid(1))
+
+
+query.delete(key)   # Delete last record inserted
+
+debugger_2(db)
+
+
+
+
