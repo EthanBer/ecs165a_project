9a249d2f44048bbfb8f9c0d4ed24178fab36e44f
ethanber <raincloud9999@outlook.com>
2024-02-26 16:30:37 -0800

    select version from a482004

--- lstore/query.py
+++ lstore/query.py
@@ -21,9 +21,9 @@ class Query:
 
     def __init__(self, table: Table):
         self.table = table
-        self.db_bpool = table.db_bufferpool # The bufferpool is the same for every table, because there is only one
+        self.db_bpool = table.db_bufferpool  # The bufferpool is the same for every table, because there is only one
         pass
-    
+
     """
     # internal Method
     # Read a record with specified RID
@@ -114,7 +114,7 @@ class Query:
     # gets the most up-to-date column value for a record.
     def get_updated_col(self, record: Record, col_idx: DataIndex) -> int | None:
         return -1
-        
+
     """
     # Read matching record with specified search key
     # :param search_key: the value you want to search based on
@@ -126,8 +126,10 @@ class Query:
     """
 
     def select(self, search_key: int, search_key_index: DataIndex,
-               projected_columns_index: list[Literal[0] | Literal[1]], use_idx: bool = False) -> list[Record] | Literal[-1]:
+               projected_columns_index: list[Literal[0] | Literal[1]], use_idx: bool = False) -> list[Record] | Literal[
+        -1]:
         return -1
+
     """
     # Read matching record with specified search key
     # :param search_key: the value you want to search based on
@@ -138,9 +140,79 @@ class Query:
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
     """
+
     def select_version(self, search_key: int, search_key_index: DataIndex, projected_columns_index: list[Literal[0, 1]],
                        relative_version: int) -> list[Record] | Literal[False]:
-        return False 
+        # search_key_index = DataIndex(search_key_index)
+        # projected_columns_index = [DataIndex(idx) for idx in projected_columns_index]
+      #  print(self.table.page_directory.keys())
+        ret: list[Record] = []
+        # if search_key is the key_index, then the rid is located by the index
+        # otherwise, locate the rid manually
+        # ...get the updated value for this rid
+        valid_records: list[Record] = []
+        if search_key_index == self.table.key_index:
+           # print(search_key_index)
+            #print(search_key)
+            rid = self.table.index.locate(search_key_index, search_key)
+            if rid is not None:
+                # raise(Exception("select should not be called on a key that doesn't exist"))
+                valid_records.append(self.table.get_record_by_rid(rid))
+        else:
+            for rid in range(1, self.table.last_rid):
+                rec = self.table.get_record_by_rid(rid)
+                if rec.base_record == False:
+                    continue
+                search_key_col = self.get_updated_col(rec, search_key_index)
+                if search_key_col == search_key:
+                    valid_records.append(rec)
+                #     valid_records.append(rid)
+
+        for record in valid_records:
+            col_list = list(record.columns)
+            schema_encoding = record.schema_encoding
+            for i in range(len(col_list)):
+                if projected_columns_index[i] == 0:
+                    col_list[i] = None
+                else:
+                    if record.rid == None:
+                        continue
+                    else:
+                        if helper.ith_bit(schema_encoding, self.table.num_columns, i,
+                                          False) == 0b1:  # check if the column has been updated.
+                            # print("detected on schema encoding bit")
+                            assert record.indirection_column is not None, "inconsistent state: schema_encoding bit on but indirection was None"
+                            curr_rid = record.indirection_column
+                            curr_schema_encoding = self.table.get_record_by_rid(curr_rid).schema_encoding
+                            counter = 0
+                            overversioned = False
+                            while counter > relative_version or helper.ith_bit(curr_schema_encoding,
+                                                                               self.table.num_columns, i, False) == 0b0:
+                                temp = self.table.get_record_by_rid(curr_rid)
+                                if temp is None:
+                                    overversioned = True
+                                    break
+                                assert temp.indirection_column is not None, "looped back to base record? indirection_column == None"
+                                curr_rid = temp.indirection_column
+                                curr_schema_encoding = self.table.get_record_by_rid(curr_rid).schema_encoding
+                                counter -= 1
+                                # curr_rid, curr_schema_encoding = temp.indirection_column, temp.schema_encoding
+                                # curr_indirection = temp
+                            if overversioned is True :
+                                curr_rid = record.indirection_column
+                            col_list[i] = self.table.get_record_by_rid(curr_rid)[i]
+                    # rec.columns[i] = None  # filter out that column from the projection
+            record.columns = tuple(col_list)
+            ret.append(record)
+
+            # for i, column_value, data_index_indicator in enumerate(zip(rec.columns, projected_columns_index)):
+            #     if data_index_indicator == 1:
+            #         rec.columns[i] = column_value
+            # return [rec]
+            # else:
+            #     return None
+        # self.table.page_ranges.
+        return ret
 
     """
     # Update a record with specified key and columns
@@ -196,8 +268,8 @@ class Query:
             # valid_records.append(self.select(key, aggregate_column_index, projected_cols)[0])
         if len(valid_numbers) == 0:
             return False
-        else: return sum(valid_numbers)
-
+        else:
+            return sum(valid_numbers)
 
     """
     incremenets one column of the record
@@ -209,7 +281,7 @@ class Query:
     """
 
     def increment(self, key: int, column: DataIndex) -> bool:
-        r = self.select(key, self.table.key_index, [1] * self.table.num_columns)[0] # type: ignore
+        r = self.select(key, self.table.key_index, [1] * self.table.num_columns)[0]  # type: ignore
         if r is not False:
             updated_columns: list[int | None] = [None] * self.table.num_columns
             to_add: int = 0
